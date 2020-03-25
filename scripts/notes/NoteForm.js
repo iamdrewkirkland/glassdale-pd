282138f0a853da4518b4ff265f6b3d964c6b63b0
iamdrewkirkland <drew.kirkland@gmail.com>
2020-03-25 10:31:45 -0400

    Criminal name has been changed to dropdown on note form.

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -1,43 +1,54 @@
 import { saveNote } from "./noteDataProvider.js";
+import { useCriminals } from "../criminals/criminalProvier.js";
 
 const contentTarget = document.querySelector(".noteFormContainer");
-const eventHub = document.querySelector(".container")
+const eventHub = document.querySelector(".container");
 
-let visbility = false
+let visbility = false;
 
+// Event handler for toggling note form visibility
 eventHub.addEventListener("noteFormButtonClicked", customEvent => {
-  visbility = !visbility                                              
+  visbility = !visbility;
 
   if (visbility) {
-    contentTarget.classList.remove("invisible")
+    contentTarget.classList.remove("invisible");
   } else {
-    contentTarget.classList.add("invisible")
+    contentTarget.classList.add("invisible");
   }
+});
 
-})
-
+// Event handler for adding (saving) a note
 contentTarget.addEventListener("click", clickEvent => {
   if (clickEvent.target.id === "saveNote") {
+    const criminalId = document.querySelector("#criminalDropdown").value;
     const noteText = document.querySelector("#note-text").value;
-    const criminalName = document.querySelector("#note-suspect").value;
     const noteDate = document.querySelector("#noteDate").value;
 
     const newNote = {
       noteText: noteText,
-      criminalName: criminalName,
-      noteDate: noteDate,
+      criminalId: parseInt(criminalId),
+      noteDate: noteDate
     };
     saveNote(newNote);
   }
 });
 
+// Render dropdown with criminals as options
 const render = () => {
-  contentTarget.classList.add("invisible")
+  contentTarget.classList.add("invisible");
+  const criminalArrray = useCriminals();
+
   contentTarget.innerHTML = `
       <fieldset>
       <legend>NOTES</legend>
         <label>Suspect Name:</label>
-        <input type="text" id="note-suspect">
+        <select id="criminalDropdown">
+          <option value="0">Select a Criminal
+          </option>
+          ${criminalArrray.map(currentCriminalObject => {
+            return `<option value ="${currentCriminalObject.id}>${currentCriminalObject.name}</option>`;
+          })}
+        </select>
         <label>Date of Entry:</label>
         <input type="date" id="note-date">
         <label>Note:</label>
--- scripts/notes/NoteList.js
+++ scripts/notes/NoteList.js
@@ -1,60 +1,60 @@
 import { Note } from "./Note.js";
 import { useNotes, getNotes, deleteNote } from "./noteDataProvider.js";
-
+import { useCriminals } from "../criminals/criminalProvier.js";
 
 // DOM Element Selectors
-const contentTarget = document.querySelector(".noteListContainer")
-const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".noteListContainer");
+const eventHub = document.querySelector(".container");
 
 // State variables
-let visbility = false
+let visbility = false;
 
 // Event handler for delete note
 contentTarget.addEventListener("click", clickEvent => {
-    if (clickEvent.target.id.startsWith("deleteNote--")) {
-      const [prefix, noteId] = clickEvent.target.id.split("--")
-      deleteNote(noteId)
-    }
-})
+  if (clickEvent.target.id.startsWith("deleteNote--")) {
+    const [prefix, noteId] = clickEvent.target.id.split("--");
+    deleteNote(noteId);
+  }
+});
 
 // Event handler for note state
 eventHub.addEventListener("noteStateChanged", customEvent => {
-  render()
-})
+  render();
+});
 
 // Event handler for note visiblity toggle
 eventHub.addEventListener("allNotesClicked", customEvent => {
-  visbility = !visbility
-  
-  if (visbility){
-    contentTarget.classList.remove("invisible")
-  } 
-  else {
-    contentTarget.classList.add("invisible")
+  visbility = !visbility;
+
+  if (visbility) {
+    contentTarget.classList.remove("invisible");
+  } else {
+    contentTarget.classList.add("invisible");
   }
-
-})                                                
-
+});
 
 const render = () => {
-
-  if (visbility){
-    contentTarget.classList.remove("invisible")
+  if (visbility) {
+    contentTarget.classList.remove("invisible");
   } else {
-    contentTarget.classList.add("invisible")
+    contentTarget.classList.add("invisible");
   }
-  
-  getNotes().then(() => {
-      const allTheNotes = useNotes()
 
-      contentTarget.innerHTML = allTheNotes.map(
-          currentNoteObject => {
-              return Note(currentNoteObject)
-          }
-      ).join("")
-  })
-}
+  getNotes().then(() => {
+    const allTheNotes = useNotes();
+    const allTheCriminals = useCriminals();
+
+    contentTarget.innerHTML = allTheNotes
+      .map(currentNoteObject => {
+        const selectedCriminal = allTheCriminals.find(currentCriminalObject => {
+          return currentNoteObject.criminal === currentCriminalObject.id;
+        });
+        return Note(currentNoteObject, selectedCriminal);
+      })
+      .join("");
+  });
+};
 
 export const NoteList = () => {
-  render()
-}
+  render();
+};
